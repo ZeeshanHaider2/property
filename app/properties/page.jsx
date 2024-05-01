edb04f71e1909e92297145f3e1f192dd224d64a8
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-05-02 01:12:38 +0200

    Property Card Dynamic Data

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,19 @@
-import Link from 'next/link';
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>PropertiesPage</h1>
-      <Link href='/'>Go Home</Link>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+      {properties.length === 0 ? (
+        <p>No property found</p>
+      ) : ( <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      {properties.map((property)=>(
+        <PropertyCard key={property._id} property={property}/>
+      ))}
+   </div>)}
+  
     </div>
+    </section>
   )
 }
 
