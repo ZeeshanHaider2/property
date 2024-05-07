e356a8aab9a35e5c7c91755da72bf1b38148b53a
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-05-07 21:29:36 +0200

    Form Input Handlers

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -35,9 +35,63 @@ const PropertyAddForm = () => {
         setMounted(true)
     }, []);
 
-  const handleChange = () => {}
-  const handleAmenitiesChange = () => {}
-  const handleImageChange = () => {}
+  const handleChange = (e) => {
+    const { name, value} = e.target;
+//Check if nested property
+    if ( name.includes('.')){
+        const [outerKey, innerKey] = name.split('.');
+
+        setFields((prevFields) => ({
+            ...prevFields,
+            [outerKey]:{
+                ...prevFields[outerKey],
+                [innerKey]: value,
+            }
+        }));
+    } else {
+        //not nested
+        setFields((prevFields)=>({
+            ...prevFields,
+            [name]: value,
+        }));
+    }
+  }
+  const handleAmenitiesChange = (e) => {
+    const { value, checked} = e.target
+    //clone the current array
+    const updatedAmenities = [ ...fields.amenities]
+    if (checked){
+        //Add value to array
+        updatedAmenities.push(value);
+    } else{
+        //Remove value from array
+        const index = updatedAmenities.indexOf(value);
+        if(index !== -1){
+            updatedAmenities.splice(index, 1);
+        }
+    }
+    //update state with updated array
+    setFields((prevFields)=> ({
+        ...prevFields,
+        amenities:updatedAmenities,
+    }))
+  };
+  const handleImageChange = (e) => {
+    const { files } = e.target;
+     // Clone images array
+     const updatedImages = [...fields.images];
+
+     // Add new files to the array
+     for (const file of files) {
+       updatedImages.push(file);
+     }
+ 
+     // Update state with array of images
+     setFields((prevFields) => ({
+       ...prevFields,
+       images: updatedImages,
+     }));
+  }
 
   return (
     mounted && (
@@ -202,7 +256,7 @@ const PropertyAddForm = () => {
                   name="amenities"
                   value="Wifi"
                   className="mr-2"
-                  checked={fields.amenities.includes('wifi')}
+                  checked={fields.amenities.includes('Wifi')}
                   onChange={handleAmenitiesChange}
                 />
                 <label htmlFor="amenity_wifi">Wifi</label>
