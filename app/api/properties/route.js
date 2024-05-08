87a0a2138f8f2221bb69fe31ba00b9a55fc55ccd
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-05-08 15:39:21 +0200

    Cloudinary Image Upload

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 //Get /api/properties
 export const GET = async (request) => {
@@ -21,6 +23,15 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
 
     //Access all values from amenities and images
@@ -54,13 +65,47 @@ export const POST = async (request) => {
         email: formData.get("seller_info.email"),
         phone: formData.get("seller_info.phone"),
       },
-      images,
+      owner: userId,
     };
-    console.log(propertyData);
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 200,
-    });
+    //Upload image(s) to cloudinary
+    const imageUploadPromises = [];
+
+    for (const image of images) {
+      const imageBuffer = await image.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      //Convert the image to base64
+      const imageBase64 = imageData.toString("base64");
+
+      //make request to upload to cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "propertyzenith",
+        }
+      );
+      imageUploadPromises.push(result.secure_url);
+
+      //Wait for all images to upload
+      const uploadedImages = await Promise.all(imageUploadPromises);
+      //Add uploaded images to the propertyData object
+      propertyData.images = uploadedImages;
+    }
+
+    //console.log(propertyData);
+    //Adding form data into the database
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+
+    // return new Response(JSON.stringify({ message: "Success" }), {
+    //   status: 200,
+    // });
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
   }
