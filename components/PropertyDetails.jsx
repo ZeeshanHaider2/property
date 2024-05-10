ea80de9c105c84ee23e30febd346e1899e56d7f5
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-05-10 17:27:40 +0200

    Property Page Cleanup

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -6,6 +6,7 @@ import {
     FaCheck,
     FaMapMarker
 } from 'react-icons/fa'
+import PropertyMap from '@/components/PropertyMap';
 
 const PropertyDetails = ({property}) => {
   return (
@@ -96,13 +97,13 @@ const PropertyDetails = ({property}) => {
                     </li>
                 ))}
                 
-              </ul>
+             </ul>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <div id="map"></div>
+              <PropertyMap property={property}/>
             </div>
           </main>
   )
 }
-
 export default PropertyDetails
